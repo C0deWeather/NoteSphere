f562f9c7d3b57876e7a7a56e68ee00dd1bd45dc4
C0deWeather <bflloyd1653@gmail.com>
2026-03-17 18:06:07 +0100

    Added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.api import api_router
 from app.core.config import settings
 from app.core.logging import setup_logging
@@ -10,6 +11,15 @@ logger = logging.getLogger("app")
 
 app = FastAPI(title="NosteSphere", debug=settings.debug, lifespan=lifespan)
 app.include_router(api_router, prefix="/api/v1")
+
+origins = settings.cors_origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.middleware("http")
 async def log_requests(request: Request, call_next):

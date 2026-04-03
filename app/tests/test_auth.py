704f2176b31da55b591b834978045acf93987a89
C0deWeather <frankbright211@gmail.com>
2026-04-03 14:27:18 +0000

    added test for succesful login

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -1,3 +1,6 @@
+from jose import jwt
+from app.core.config import settings
+
 def test_succesful_signup(client, signup_payload):
     response = client.post("api/v1/auth/signup", json=signup_payload)
     assert response.status_code == 201
@@ -11,4 +14,18 @@ def test_signup_with_existing_email(client, signup_payload):
     response = client.post("api/v1/auth/signup", json=signup_payload)
     assert response.status_code == 400
     assert response.json()["detail"] == "Email already exists"
+
+def test_successful_login(client, login_payload, signup_payload):
+    # Create user
+    client.post("api/v1/auth/signup", json=signup_payload)
+
+    # Verify login
+    response = client.post("api/v1/auth/login", json=login_payload)
+    assert response.status_code == 200
+
+    # Verify jwt
+    token = response.json()["access_token"]
+    payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+    assert payload["sub"] == login_payload["email"]
+
     
